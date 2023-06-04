86003be74af786b10c78da38f9485f174a4c3ec1
Ion Pascari <ionpascari1995@gmail.com>
2023-06-04 19:55:38 +0300

    feat: create the skeleton of the app

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.1.0"
+	id("io.spring.dependency-management") version "1.1.0"
+}
+
+group = "inc.evil"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+repositories {
+	mavenCentral()
+}
+
+extra["springCloudVersion"] = "2022.0.3"
+
+dependencies {
+	implementation("org.springframework.cloud:spring-cloud-starter-gateway")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/test/java/inc/evil/cnsedgeservice/CnsEdgeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package inc.evil.cnsedgeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnsEdgeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
